158c156c18293f99c444e3b3d03843d6f81612d8
Emmanuel Joseph Esema <emmanuje@ulrik.uio.no>
2022-07-13 12:03:52 +0200

    Updated program to take calculate D.O.B. instead of typing in age

--- school_register.py
+++ school_register.py
@@ -1,9 +1,13 @@
 import numpy as np
 import matplotlib.pyplot as plt
+import datetime
+
 
 class Student:
-    def __init__(self, name):
-        self.name = name
+    def __init__(self):
+        self.name = input("Name of Student: ")
+        name = self.name
+        self.dt = datetime.datetime.now()   #date today
         
         for i in range(1,len(name)):
             if (name[i] == " "):
@@ -16,7 +20,7 @@ class Student:
     def stdnt_info(self):
         
         """
-        Creating and Saving Student Information
+        Creating and Saving Student Information in dictionaries
 
         Returns
         -------
@@ -26,14 +30,68 @@ class Student:
         
         fst_name = self.fst_name
         lst_name = self.lst_name
-        self.age = int(input('Enter Student age: '))
+        dt = self.dt #date today
+        
+        self.DOB = input("Enter Date of Birth (dd.mm.yyyy): ")
+        DOB = self.DOB
+        
+        for i in range(len(DOB)):
+           if (DOB[i] == "."):
+               self.birth_day = (DOB[:i-3])
+               self.birth_month = (DOB[3:i])
+               self.birth_year = (DOB[i+1:])
+               
+
+        birth_day = int(self.birth_day)
+        birth_month = int(self.birth_month)
+        birth_year = int(self.birth_year)
+        
+        #print("dd", birth_day, "mm", birth_month, "yyyy", birth_year)
+        
+        if birth_day < 1 or birth_day > 31:
+            raise ValueError ("Invalid Input! Birth month is " 
+                              "less/greater than 1 or 31 respectively")
+            
+        if birth_month < 1 or birth_month > 12:
+            raise ValueError ("Invalid Input! Birth month is " 
+                              "less/greater than 1 or 12 respectively")
+            
+        if birth_year > dt.year:
+            raise ValueError ("Invalid Input! Birth Year is greater than current year", dt.year)
+        
+        
+        if birth_month < 10:
+            #self.DOB = int("%s%s%s"  %(birth_day, birth_month, birth_year ))
+            self.DOB = int(f"{birth_day}0{birth_month}{birth_year}")
+        else:
+            self.DOB = int(f"{birth_day}{birth_month}{birth_year}")
+
+        DOB = self.DOB
+        
+        
+        self.age = dt.year - birth_year
+        
+        if dt.month < birth_month or dt.month == birth_month and dt.day < birth_day:
+            self.age -= 1
+            
         age = self.age
 
         
-        info = {"First Name": fst_name, "Last Name": lst_name, "Age": age}
+    
+        info = {"First Name": fst_name, 
+                "Last Name": lst_name, 
+                "Age": age,  
+                "D.O.B.": DOB}
+        
+        print()
+        print("Today's date: %s/%s/%s"  %(dt.day, dt.month, dt.year ))
+        print()
         
         return info
         
 if __name__ == '__main__':
-    Student("Bobby Esema")
-    Student("James Etokebe")
+    #Student("Bobby Esema")
+    #Student("James Etokebe")
+    Student()
+
+
